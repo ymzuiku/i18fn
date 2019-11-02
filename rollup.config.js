7219e30cebf7bb129b913b2ad3e4cc0edf0a48b0
梁柱 <ymblender@gmail.com>
2019-11-02 15:40:29 +0800

    3

--- rollup.config.js
+++ rollup.config.js
@@ -37,7 +37,7 @@ const watchOptions = [
     output: {
       file: './umd/index.js',
       format: 'umd',
-      name: 'vanillaNormalizeCss',
+      name: 'i18fn',
       sourcemap: true,
       globals: {
         // queryString: 'querystring-number',
